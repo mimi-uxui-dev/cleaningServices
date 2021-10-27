9c50fdba5654648bde3be03cb47c7c4f8f5c3200
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-10-27 09:53:40 +0100

    updates fixing warnings

--- src/components/AboveTheFold.js
+++ src/components/AboveTheFold.js
@@ -14,12 +14,12 @@ function AboveTheFold() {
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla ad molestiae quaerat, fugit illum nam explicabo ullam nostrum.
             </p>
             <div className="flex flex-row gap-8 items-center mt-16 ">
-                <a href="" className="flex flex-row gap-4 items-center justify-start font-medium tracking-wide text-sm rounded-lg text-bgg bg-bluue hover:bg-darkBluue transition-all px-4 py-2" >
-                        <img src={calendar} className="w-6" alt="CS" />
-                        Book a Schedule
-                    
+                <a href="/" className="flex flex-row gap-4 items-center justify-start font-medium tracking-wide text-sm rounded-lg text-bgg bg-bluue hover:bg-darkBluue transition-all px-4 py-2" >
+                    <img src={calendar} className="w-6" alt="CS" />
+                    Book a Schedule
+
                 </a>
-                <a href="" className="flex flex-row gap-4 items-center text-darkBluue font-semibold hover:text-bluue transition-all">
+                <a href="/" className="flex flex-row gap-4 items-center text-darkBluue font-semibold hover:text-bluue transition-all">
                     <img src={play} className='w-10' alt="CS" />
                     Watch Video
                 </a>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,11 +5,7 @@ import p3 from '../assets/imgs/p3.png'
 import p4 from '../assets/imgs/p4.png'
 import arrow from '../assets/imgs/arr_w.svg'
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination, Navigation, Autoplay } from 'swiper';
-
 function Portfolio() {
-    SwiperCore.use([Pagination, Navigation]);
     return (
         <div className='bg-bgg border-yelloww flex flex-col justify-center items-center py-24'  >
             <p className="text-sm text-piink font-semibold">Our Portfolio</p>
@@ -20,7 +16,7 @@ function Portfolio() {
             <div className='flex flex-row gap-8 w-full mt-8 px-4'>
 
                 <div className="flex flex-col gap-2 relative top-0 left-0">
-                    <img src={p1} className='rounded-md ' alt="" />
+                    <img src={p1} className='rounded-md ' alt="CS" />
                     <div className='absolute bottom-0 left-0 p-4 text-bgg'>
                         <h4 className='text-2xl font-semibold pb-2'>Office Cleaning</h4>
                         <p className='text-sm tracking-wide' >Commercial Service</p>
@@ -28,7 +24,7 @@ function Portfolio() {
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 relative top-0 left-0">
-                    <img src={p2} className='rounded-md ' alt="" />
+                    <img src={p2} className='rounded-md ' alt="CS" />
                     <div className='absolute bottom-0 left-0 p-4 text-bgg'>
                         <h4 className='text-2xl font-semibold pb-2'>Office Cleaning</h4>
                         <p className='text-sm tracking-wide' >Commercial Service</p>
@@ -36,7 +32,7 @@ function Portfolio() {
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 relative top-0 left-0">
-                    <img src={p3} className='rounded-md ' alt="" />
+                    <img src={p3} className='rounded-md ' alt="CS" />
                     <div className='absolute bottom-0 left-0 p-4 text-bgg'>
                         <h4 className='text-2xl font-semibold pb-2'>Office Cleaning</h4>
                         <p className='text-sm tracking-wide' >Commercial Service</p>
@@ -44,14 +40,14 @@ function Portfolio() {
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 relative top-0 left-0">
-                    <img src={p4} className='rounded-md ' alt="" />
+                    <img src={p4} className='rounded-md ' alt="CS" />
                     <div className='absolute bottom-0 left-0 p-4 text-bgg'>
                         <h4 className='text-2xl font-semibold pb-2'>Office Cleaning</h4>
                         <p className='text-sm tracking-wide' >Commercial Service</p>
                         <img src={arrow} className='w-8 mt-4' alt="CS" />
                     </div>
                 </div>
-                
+
             </div>
 
         </div>
